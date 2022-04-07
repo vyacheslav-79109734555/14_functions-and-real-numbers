debcb2bdf7f4ea2df971b0ce1df7023e8e4a83a5
Вячеслав <95027060+vyacheslav-79109734555@users.noreply.github.com>
2022-04-07 19:03:47 +0300

    Сессия

---
+++ Сессия/Сессия.py
@@ -0,0 +1,20 @@
+# Задача 2. Сессия
+
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+x_diff = x1 - x2
+y_diff = y1 - y2
+if x_diff == 0 or y_diff == 0:
+    k = 1
+    b = y2 - k * x2
+else:
+    k = y_diff / x_diff
+    b = y2 - k * x2
+
+print("Уравнение прямой, проходящей через эти точки:")
+print("y = ", k, " * x + ", b)
